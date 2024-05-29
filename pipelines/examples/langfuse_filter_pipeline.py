69be71ea4c6544c8b8e2797bdacaabba198c0b6c
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-05-28 18:17:49 -0700

    fix: langfuse

--- pipelines/examples/langfuse_filter_pipeline.py
+++ pipelines/examples/langfuse_filter_pipeline.py
@@ -2,6 +2,8 @@ from typing import List, Optional
 from schemas import OpenAIChatMessage
 import os
 
+
+from pydantic import BaseModel
 from langfuse import Langfuse
 from langfuse.decorators import langfuse_context, observe
 
@@ -19,32 +21,38 @@ class Pipeline:
         self.id = "langfuse_filter_pipeline"
         self.name = "Langfuse Filter"
 
-        # Assign a priority level to the filter pipeline.
-        # The priority level determines the order in which the filter pipelines are executed.
-        # The lower the number, the higher the priority.
-        self.priority = 0
+        class Valves(BaseModel):
+            # List target pipeline ids (models) that this filter will be connected to.
+            # If you want to connect this filter to all pipelines, you can set pipelines to ["*"]
+            pipelines: List[str] = []
 
-        # List target pipelines (models) that this filter will be connected to.
-        # If you want to connect this filter to all pipelines, you can set pipelines to ["*"]
-        self.pipelines = ["*"]
+            # Assign a priority level to the filter pipeline.
+            # The priority level determines the order in which the filter pipelines are executed.
+            # The lower the number, the higher the priority.
+            priority: int = 0
 
-        self.secret_key = os.getenv("LANGFUSE_SECRET_KEY")
-        self.public_key = os.getenv("LANGFUSE_PUBLIC_KEY")
-        self.host = os.getenv("LANGFUSE_HOST")
+            # Valves
+            secret_key: str
+            public_key: str
+            host: str
 
-        self.langfuse = Langfuse(
-            secret_key=self.secret_key,
-            public_key=self.public_key,
-            host=self.host,
-            debug=True,
+        # Initialize
+        self.valves = Valves(
+            **{
+                "pipelines": ["*"],  # Connect to all pipelines
+                "secret_key": os.getenv("LANGFUSE_SECRET_KEY"),
+                "public_key": os.getenv("LANGFUSE_PUBLIC_KEY"),
+                "host": os.getenv("LANGFUSE_HOST", "https://cloud.langfuse.com"),
+            }
         )
 
-        self.langfuse.auth_check()
+        self.langfuse = None
         pass
 
     async def on_startup(self):
         # This function is called when the server is started or after valves are updated.
         print(f"on_startup:{__name__}")
+        self.set_langfuse()
         pass
 
     async def on_shutdown(self):
@@ -52,6 +60,19 @@ class Pipeline:
         print(f"on_shutdown:{__name__}")
         self.langfuse.flush()
         pass
+
+    async def on_valves_update(self):
+        self.set_langfuse()
+        pass
+
+    def set_langfuse(self):
+        self.langfuse = Langfuse(
+            secret_key=self.valves.secret_key,
+            public_key=self.valves.public_key,
+            host=self.valves.host,
+            debug=True,
+        )
+        self.langfuse.auth_check()
 
     async def filter(self, body: dict, user: Optional[dict] = None) -> dict:
         print(f"filter:{__name__}")
