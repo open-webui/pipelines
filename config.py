a2e11db46fbc19289d663104f7697e3ffc02444c
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-05-31 19:40:02 -0700

    refac

--- config.py
+++ config.py
@@ -10,3 +10,6 @@ try:
     load_dotenv(find_dotenv("./.env"))
 except ImportError:
     print("dotenv not installed, skipping...")
+
+API_KEY = os.getenv("PIPELINES_API_KEY", "0p3n-w3bu!")
+PIPELINES_DIR = os.getenv("PIPELINES_DIR", "./pipelines")
--- main.py
+++ main.py
@@ -27,20 +27,7 @@ import uuid
 import sys
 
 
-####################################
-# Load .env file
-####################################
-
-try:
-    from dotenv import load_dotenv, find_dotenv
-
-    load_dotenv(find_dotenv(".env"))
-except ImportError:
-    print("dotenv not installed, skipping...")
-
-API_KEY = os.getenv("PIPELINES_API_KEY", "0p3n-w3bu!")
-
-PIPELINES_DIR = os.getenv("PIPELINES_DIR", "./pipelines")
+from config import API_KEY, PIPELINES_DIR
 
 if not os.path.exists(PIPELINES_DIR):
     os.makedirs(PIPELINES_DIR)
