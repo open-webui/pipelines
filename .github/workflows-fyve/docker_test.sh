8aedf0d7b43115016891b14c95efc83907b9f67f
Eric Z <ezsomething@gmail.com>
2024-12-30 08:15:52 -0600

    fix(testing): minor tweak for testing via docker script

--- .github/workflows-fyve/docker_test.sh
+++ .github/workflows-fyve/docker_test.sh
@@ -26,12 +26,14 @@ rm -rf $WORKING_PATH/coverage
 mkdir -p $WORKING_PATH/coverage
 
 # Run the Docker container with the required environment variables
-docker run --rm -t -e KEY_PATH="$KEY_PATH" -v "$DATA_PATH":/data -v "$WORKING_PATH/coverage:/coverage" -v "$WORKING_PATH/tests:/app/tests" $IMAGE_NAME:dev pytest --cov=$IMAGE_NAME --cov-report html:/coverage/coverage.html --cov-report=xml:/coverage/coverage.xml tests
+# docker run --rm -t -e KEY_PATH="$KEY_PATH" -v "$DATA_PATH":/data -v "$WORKING_PATH/coverage:/coverage" -v "$WORKING_PATH/tests:/app/tests" $IMAGE_NAME:dev pytest --cov=$IMAGE_NAME --cov-report html:/coverage/coverage.html --cov-report=xml:/coverage/coverage.xml tests
+docker run --rm -t -e KEY_PATH="$KEY_PATH" -v "$DATA_PATH":/data -v "$WORKING_PATH/coverage:/coverage" -v "$WORKING_PATH/tests:/app/tests" $IMAGE_NAME:dev pytest --cov=/app  --cov-report html:/coverage/coverage.html --cov-report=xml:/coverage/coverage.xml tests
 
 # stop the build if there are Python syntax errors or undefined names
 SELECT_CLAUSE="--ignore=E501,E121,E128,E124,E123"
 # SELECT_CLAUSE=--select=E9,F63,F7,F82   # limit errors?
-docker run --rm -t -e KEY_PATH="$KEY_PATH" -v "$DATA_PATH":/data -v "$WORKING_PATH/coverage:/coverage" $IMAGE_NAME:dev flake8 /app/$IMAGE_NAME --exclude .venv --count $SELECT_CLAUSE --show-source --statistics --output-file=/coverage/flake8.txt --color=never --exit-zero
+# docker run --rm -t -e KEY_PATH="$KEY_PATH" -v "$DATA_PATH":/data -v "$WORKING_PATH/coverage:/coverage" $IMAGE_NAME:dev flake8 /app/$IMAGE_NAME --exclude .venv --count $SELECT_CLAUSE --show-source --statistics --output-file=/coverage/flake8.txt --color=never --exit-zero
+docker run --rm -t -e KEY_PATH="$KEY_PATH" -v "$DATA_PATH":/data -v "$WORKING_PATH/coverage:/coverage" $IMAGE_NAME:dev flake8 /app --exclude .venv --count $SELECT_CLAUSE --show-source --statistics --output-file=/coverage/flake8.txt --color=never --exit-zero
 
 
 # final coverage report available
