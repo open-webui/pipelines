ef52d542554baf8e810a6c45521a281920e2a3ea
Eric Z <ezsomething@gmail.com>
2025-04-12 17:16:39 -0500

    fix(undefined-const): hotfix for missing constants; hazarding guess from context but breaks build

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
+import logging
 ####################################
 # Load .env file
 ####################################
@@ -10,6 +10,15 @@ try:
     load_dotenv(find_dotenv("./.env"))
 except ImportError:
     print("dotenv not installed, skipping...")
+
+# Define log levels dictionary
+LOG_LEVELS = {
+    'DEBUG': logging.DEBUG,
+    'INFO': logging.INFO,
+    'WARNING': logging.WARNING,
+    'ERROR': logging.ERROR,
+    'CRITICAL': logging.CRITICAL
+}
 
 API_KEY = os.getenv("PIPELINES_API_KEY", "0p3n-w3bu!")
 PIPELINES_DIR = os.getenv("PIPELINES_DIR", "./pipelines")
